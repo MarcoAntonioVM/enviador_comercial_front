b778aa60f7fd1a1c6b3935817922976c08d2ea8a
Guillermo Isidoro <guillermo.isidoro.efinfo@gmail.com>
2026-01-19 16:45:53 -0600

    Add TemplateFormPage component to render entity form for templates

---
+++ src/features/templates/pages/TemplateFormPage.tsx
@@ -0,0 +1,6 @@
+import { EntityFormPage } from "@/components/entity/EntityFormPage";
+import { templateFormConfig } from "../template.form";
+
+export default function TemplateFormPage() {
+  return <EntityFormPage config={templateFormConfig} />;
+}
