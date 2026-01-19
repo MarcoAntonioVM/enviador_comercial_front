9977d93ee53f36fd951c6f8b6f1f1b704d889678
Guillermo Isidoro <guillermo.isidoro.efinfo@gmail.com>
2026-01-19 10:59:51 -0600

    Add AppConfirmDialog component to handle confirmation dialogs

---
+++ src/components/ConfirmDialog/AppConfirmDialog.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ConfirmDialog } from 'primereact/confirmdialog';
+
+export const AppConfirmDialog: React.FC = () => {
+    return (
+        <ConfirmDialog />
+    );
+};
